c40ba191f7bf80195bd7e5b1369bca0b16cf0637
moonheart08 <moonheart08@users.noreply.github.com>
2024-09-19 16:09:34 -0500

    OC literally just died.

--- setup.lua
+++ setup.lua
@@ -1,4 +1,5 @@
 local internet = require("internet")
+local fs = require("filesystem")
 local branch = "master"
 local remoteFilesUrl = "https://raw.githubusercontent.com/moonheart08/gtnh-oc/"..branch.."/"
 
@@ -14,10 +15,6 @@ local function grabFile(url)
 	
 	local code, message, headers = mt.__index.response()
 	
-	if tostring(code) != "200" then
-		error("Expected a 200 response when fetching " .. url .. " but got " .. code .. "instead.")
-	end
-	
 	return result
 end
 
@@ -28,6 +25,9 @@ local repoMap = {
 
 -- OC computers don't have particularly much disk space, much less RAM, so I opt to not try to cache the changes in memory before applying.
 for entry in repoMap do
-	local url = remoteFilesUrl .. entry[1]
-	
+	local url = remoteFilesUrl .. entry[2]
+	local data = grabFile(url)
+	local handle = fs.open(entry[1], "wb")
+	handle:write(data)
+	handle:close()
 end
