efc4d6d87c6b0bd811cac920d2f0f667bdc76e7a
moonheart08 <moonheart08@users.noreply.github.com>
2024-10-05 05:03:09 -0500

    A classic case of misremembering the JSON spec.

--- disk_template/tests/json.lua
+++ disk_template/tests/json.lua
@@ -3,18 +3,18 @@ local package = require("package")
 local asserteq = require("testing").asserteq
 local json = require("json")
 
-asserteq(table.pack(json._guessValueType({}, "123", 1))[2],"number")
-asserteq(table.pack(json._guessValueType({}, "{}", 1))[2],"table")
-asserteq(table.pack(json._guessValueType({}, "[]", 1))[2],"list")
-asserteq(table.pack(json._guessValueType({}, "\"\"", 1))[2],"string")
+asserteq(table.pack(json._guessValueType({}, "123", 1))[2], "number")
+asserteq(table.pack(json._guessValueType({}, "{}", 1))[2], "table")
+asserteq(table.pack(json._guessValueType({}, "[]", 1))[2], "list")
+asserteq(table.pack(json._guessValueType({}, "\"\"", 1))[2], "string")
 asserteq(json._deserialize({}, "0x1A"), 0x1A)
 asserteq(json._deserialize({}, "0.1"), 0.1)
 asserteq(json._deserialize({}, "\"abcdef\\u0020\\t\""), "abcdef \t")
-asserteq(json._deserialize({}, "[1, 2, 3]"), {1, 2, 3})
-asserteq(json._deserialize({}, "[1, [1, 2], 3]"), {1, {1, 2}, 3})
-asserteq(json._deserialize({}, "{\"foo\" = 1}"), {foo = 1})
-asserteq(json._deserialize({}, "{\"foo\" = 1, \"bar\" = \"baz\"}"), {foo = 1, bar = "baz"})
+asserteq(json._deserialize({}, "[1, 2, 3]"), { 1, 2, 3 })
+asserteq(json._deserialize({}, "[1, [1, 2], 3]"), { 1, { 1, 2 }, 3 })
+asserteq(json._deserialize({}, "{\"foo\": 1}"), { foo = 1 })
+asserteq(json._deserialize({}, "{\"foo\": 1, \"bar\": \"baz\"}"), { foo = 1, bar = "baz" })
 
-local test = {array = {1, 2, 3}, str = "among us.\n", table = {foo = "bar", baz = "qux"}}
+local test = { array = { 1, 2, 3 }, str = "among us.\n", table = { foo = "bar", baz = "qux" } }
 local ser = json.serialize(test)
 asserteq(test, json.deserialize(ser))
